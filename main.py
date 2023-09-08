75bf589db6ccffb60328b052d2890c7a35e3cab1
Eli Hansen <15258789+emh68@users.noreply.github.com>
2023-09-07 19:46:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ weight = float(input("Please enter your weight in lbs: "))
 BMI = int((weight / height ** 2) * 703)
 
 if BMI >= 40:
-  print(f"Your BMI is {BMI} and you are very obese.")
+  print(f"Your BMI is {BMI} you are very obese.")
 elif 30 <= BMI <= 39:
-  print(f"Your BMI is {BMI} and you are obese.")
+  print(f"Your BMI is {BMI} you are obese.")
 elif 25 <= BMI <= 29:
-  print(f"Your BMI is {BMI} and you are overweight.")
+  print(f"Your BMI is {BMI} you are overweight.")
 elif 19 <= BMI <= 24:
-  print(f"Your BMI is {BMI} and you are normal weight.")
+  print(f"Your BMI is {BMI} you are normal weight.")
 else:
-  print(f"Your BMI is {BMI} and you are underweight.")
+  print(f"Your BMI is {BMI} you are underweight.")
